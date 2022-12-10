c43d00a025f55a930d5e0faf64dc4f8351747ba8
Тележкин Владимир Алексеевич <320143@montagg.ru>
2022-12-10 15:03:20 +0500

    точно готово

--- exam.js
+++ exam.js
@@ -1,31 +1,70 @@
-export class Exam {
-    constructor(name, surname, BDball, JSball, PHPball){
+// export class Exam {
+//     constructor(name, surname, BDball, JSball, PHPball){
+//         this.name = name;
+//         this.surname = surname;
+//         this.BDball =BDball;
+//         this.JSball = JSball;
+//         this.PHPball = PHPball;
+//     }
+//     toString (){
+//         return `Фаимлмия - ${this.surname}, Имя - ${this.name}, Оценка по БД - ${this.BDball}, оценка по JS - ${this.JSball}, оценка по PHP -${this.PHPball}`
+//     }
+//     get srBall (){
+//         return (this.BDball+this.JSball+this.PHPball)/3
+//     }
+//     get xorosh (){
+//         if(this.BDball>=4 && this.JSball>=4 && this.PHPball >= 4 ){
+//             return `${this.surname} ${this.name} - хорошист`
+//         }
+//         else {
+//             return false
+//         }
+//     }
+//     get dolg (){
+//         if(this.BDball <= 2 || this.JSball <=2 || this.PHPball <= 2){
+//             return `${this.name} - должник`
+//         }
+//         else {
+//             return false
+//         }
+//     }
+// }
+export function Exam (name, surname, BDball, JSball, PHPball) {
         this.name = name;
         this.surname = surname;
         this.BDball =BDball;
         this.JSball = JSball;
         this.PHPball = PHPball;
-    }
+}
+const Exam2Metods = {
     toString (){
         return `Фаимлмия - ${this.surname}, Имя - ${this.name}, Оценка по БД - ${this.BDball}, оценка по JS - ${this.JSball}, оценка по PHP -${this.PHPball}`
     }
-    get srBall (){
-        return (this.BDball+this.JSball+this.PHPball)/3
-    }
-    get xorosh (){
-        if(this.BDball>=4 && this.JSball>=4 && this.PHPball >= 4 ){
-            return `${this.surname} ${this.name} - хорошист`
-        }
-        else {
-            return false
-        }
-    }
-    get dolg (){
-        if(this.BDball <= 2 || this.JSball <=2 || this.PHPball <= 2){
-            return `${this.name} - должник`
-        }
-        else {
-            return false
-        }
-    }
 }
+Object.assign(Exam.prototype, Exam2Metods)
+Object.defineProperties(Exam.prototype, {
+    srBall : {
+        get() {  return (this.BDball+this.JSball+this.PHPball)/3}
+    },
+    xorosh : {
+        get (){
+            if(this.BDball>=4 && this.JSball>=4 && this.PHPball >= 4 ){
+                return `${this.surname} ${this.name} - хорошист`
+            }
+            else {
+                return false
+            }
+        }
+    },
+    dolg : {
+        get(){
+            if(this.BDball <= 2 || this.JSball <=2 || this.PHPball <= 2){
+                return `${this.name} - должник`
+            }
+            else {
+                return false
+            }
+        }
+    }
+})
+
